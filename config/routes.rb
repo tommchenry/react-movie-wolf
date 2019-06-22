cc4480de4887363b79f7fc8418ff5b793e5eb5b0
Tom McHenry <tommchenry@gmail.com>
2019-06-21 20:39:14 -0500

    Make API suggest a random owned movie

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   scope '/api' do
+    get 'movies/suggest', to: 'movies#suggest_movie'
     resources :movies
     resources :directors
   end
