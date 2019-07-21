c294d43e66c5c1cd4df2f0a08b172a1925fdefe6
Tom McHenry <tommchenry@gmail.com>
2019-07-21 18:14:05 -0500

    Filter movies by title

--- client/src/movielist.js
+++ client/src/movielist.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react'
-import { Container, Item } from 'semantic-ui-react'
+import { Container, Item, Input } from 'semantic-ui-react'
 import MovieCard from './moviecard.js'
 
 class MovieList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filtered_movies: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({
+      filtered_movies: this.props.movies
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      filtered_movies: nextProps.movies
+    });
+  }
+
+  handleChange(e) {
+    let currentList = [];
+    let filteredList = [];
+
+    if (e.target.value !== "") {
+      currentList = this.props.movies;
+
+      filteredList = currentList.filter(movie => {
+        const lc = movie.title.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lc.includes(filter);
+      });
+    } else {
+      filteredList = this.props.movies;
+    }
+
+    this.setState({
+      filtered_movies: filteredList
+    });
+  }
   render() {
-    const movies = this.props.movies
+    const movies = this.state.filtered_movies
     const getYearMovies = this.props.getYearMovies
     const getDirectorMovies = this.props.getDirectorMovies
     const getFilteredMovies = this.props.getFilteredMovies
@@ -12,6 +53,7 @@ class MovieList extends Component {
     if (movies && movies.length) {
       return (
         <Item.Group divided unstackable>
+          <Input fluid icon='search' onChange={this.handleChange} placeholder='Search...' />
           {Object.keys(movies).map((key) => (
             <MovieCard key={key} movie={movies[key]} getYearMovies={getYearMovies} getDirectorMovies={getDirectorMovies} getFilteredMovies={getFilteredMovies} />
           ))}
